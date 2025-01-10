0736b5864e42833e38033b83121d4f1201e0252c
argrajujo <argrajujo@gmail.com>
2025-01-10 12:00:26 -0500

    argrajujo.py
    primer ejercicio

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/argrajujo.py
@@ -0,0 +1,77 @@
+"""
+ Mi nombre es Jujo y el lenguaje de programacion que he escogido es Python, 
+ su web oficial es https://www.python.org/ y la version que estoy utilizando es la 3.8.5.
+"""
+# Representar comentarios en Python
+
+# Sintaxis de una sola linea
+
+"""
+Sintaxis de 
+varias lineas
+"""
+
+# Representar variables en Python
+
+EDAD = 27
+FECHA_ACTUAL = "10 enero del 2025"
+
+print (f"Mi edad es {EDAD} y este es el dia {FECHA_ACTUAL}")
+
+# Representar tipos de datos en Python
+
+# Enteros
+
+int = 10 > 6
+
+# Flotantes
+
+float = 10.6
+
+# complex
+
+complex = 6j + 10
+
+# String
+
+str = "Hola mundo"
+
+# Boolean
+
+bool = True
+bool = False
+
+# Secuencias
+
+list = [8, 16, 32, 64, 128]
+tuple = (8, 16, 32, 64, 128)
+range = range(5), range(0, 10, 2), range(0, 10)
+
+# Mapeos
+
+dict = {"nombre": "Jujo", "edad": 27, "ciudad": "Cali"} 
+
+# Conjuntos
+
+set = {8, 16, 32, 64, 128} 
+
+# Frozensets
+
+frozenset = frozenset({8, 16, 32, 64, 128})
+
+# Bytes
+
+b = b"Hola"
+b = bytes(5)
+b = bytes([72, 111, 108, 97, 33])
+
+# Bytearray
+
+bytearray = bytearray(5)
+
+print (f"¡Hola mundo, soy Jujo y uso Python!")
+
+
+
+
+
